4a5f0e83eef9b3ff48b7ca4f5c364aaae6e4227f
AdonayDereje <81191245+AdonayDereje@users.noreply.github.com>
2023-11-18 22:13:32 +0300

    update css

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -6,7 +6,7 @@ function Welcome(){
     return(
         <div className="welcome">
             <img className="welcome-bg-pattern" src={backgroundPattern} alt="people"/>
-            <h1 className="welcome-mainTitle">Connecting Volunteers Around the World</h1>
+            <h1 className="welcome-mainTitle">Connecting Volunteers around the world</h1>
             <p className="welcome-subTitle">Find Your Match. Volunteer. Impact. </p>
             <button className="button welcome-button">Sign Up</button>
             <img className="welcome-img" src={welcomeImg} alt="people"/>
