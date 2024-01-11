4339811638337e8ed9e762e0966cdde2e0aaac7d
AdonayDereje <81191245+AdonayDereje@users.noreply.github.com>
2024-01-11 11:05:36 +0300

    Email Us css update

--- src/components/EmailUs.js
+++ src/components/EmailUs.js
@@ -8,10 +8,10 @@ function EmailUs (){
         <div className="review-container emailUs-container">      
             <h3 className="emailUs-mainText"><img src={Discover} width="25px" alt=""/> Discover</h3>
             <p className="emailUs-subText">Something I'm writing becuase I have the loreum ipsum thing and i'll be repearing this again and again many times.</p>
-            <h3 className="emailUs-mainText"><img src={Volunteer} width="18px" alt=""/> Volunteer</h3>
+            {/* <h3 className="emailUs-mainText"><img src={Volunteer} width="18px" alt=""/> Volunteer</h3>
             <p className="emailUs-subText">Something I'm writing becuase I have the loreum ipsum thing and i'll be repearing this again and again many times. s again and again many times.</p>
             <h3 className="emailUs-mainText"><img src={Repeat} width="25px" alt=""/> Repeat</h3>
-            <p className="emailUs-subText">Something I'm writing becuase I have the loreum ipsum thing and i'll be repearing this again and again many timess any times. times.</p>
+            <p className="emailUs-subText">Something I'm writing becuase I have the loreum ipsum thing and i'll be repearing this again and again many timess any times. times.</p> */}
         </div>
 
         
