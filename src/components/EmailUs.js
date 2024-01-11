aa27479f6d45bda1754a9a9f679d2a2721806edc
AdonayDereje <81191245+AdonayDereje@users.noreply.github.com>
2024-01-11 15:56:04 +0300

    css edit 3

--- src/components/EmailUs.js
+++ src/components/EmailUs.js
@@ -5,23 +5,30 @@ import Discover from '../discover.svg'
 
 function EmailUs (){
     return (
-        <div className="review-container emailUs-container"> 
-            <div className="emailUs-element">
-                <h3 className="emailUs-mainText"><img src={Discover} width="25px" alt=""/> Discover</h3>
-                <p className="emailUs-subText">Something I'm writing becuase I have the loreum ipsum thing and i'll be repearing this again and again many times.</p>
+        <div className='emailUs-container review-container'>
+            <div className=" emailUs-elementContainer"> 
+                <div className="emailUs-element">
+                    <h3 className="emailUs-mainText"><img src={Discover} width="25px" alt=""/> Discover</h3>
+                    <p className="emailUs-subText">Something I'm writing becuase I have the loreum ipsum thing and i'll be repearing this again and again many times.</p>
+                </div>
+                <div className="emailUs-element">
+                    <h3 className="emailUs-mainText"><img src={Volunteer} width="18px" alt=""/> Volunteer</h3>
+                    <p className="emailUs-subText">Something I'm writing becuase I have the loreum ipsum thing and i'll be repearing this again and again many times. s again and again many times.</p>
+                </div>
+                <div className="emailUs-element">
+                    <h3 className="emailUs-mainText"><img src={Repeat} width="25px" alt=""/> Repeat</h3>
+                    <p className="emailUs-subText">Something I'm writing becuase I have the loreum ipsum thing and i'll be repearing this again and again many timess any times. times.</p>
+                </div> 
             </div>
-            <div className="emailUs-element">
-                <h3 className="emailUs-mainText"><img src={Volunteer} width="18px" alt=""/> Volunteer</h3>
-                <p className="emailUs-subText">Something I'm writing becuase I have the loreum ipsum thing and i'll be repearing this again and again many times. s again and again many times.</p>
+            <div className="emailUs-buttonContainer">
+                <button className="button emailUs-button">Sign Up</button>
+                <button className="button emailUs-button whiteBg">Chat With Us</button>
             </div>
-            <div className="emailUs-element">
-                <h3 className="emailUs-mainText"><img src={Repeat} width="25px" alt=""/> Repeat</h3>
-                <p className="emailUs-subText">Something I'm writing becuase I have the loreum ipsum thing and i'll be repearing this again and again many timess any times. times.</p>
-            </div>     
+            
         </div>
-
         
     )
 }
+
 
 export default EmailUs;
